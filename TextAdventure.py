c547480cf1b1a5defe9968cf337f3dd21b42ad79
Lee Damil <lidamil6785@gmail.com>
2024-08-28 03:10:43 +0600

    add: Player stats

--- TextAdventure.py
+++ TextAdventure.py
@@ -21,6 +21,11 @@ def typeText(text, speed):
         time.sleep(delay)
 
 class Player:
+    health = 100
+    min_attack = 10
+    max_attack = 20
+    defense = 0
+    luck = 0
     coordinates = [(0,0)]
     
     @classmethod
@@ -57,8 +62,8 @@ class Player:
         else:
             pass
     
-    @classmethod
-    def die(self):
+    @staticmethod
+    def die():
         typeText("\n\nYou are dead...\n", 0)
         typeText("\nDo you want to restart?(Y/N): ",1)
         choice = input().lower()
@@ -67,29 +72,31 @@ class Player:
         else:
             print(".....")
             
-# class Item:
-#     def items(self, chest):
-#         if random.choices(["lucky", "unlucky"],[self.luck,1]) == "lucky" or chest > 0:
-#             itms = {"Gauntlet":20, "Sword":10, "Shield":8, "Necklace":6, "Stone":4, "Fruit":2}
-#             attributes = ["Strength", "Health", "Luck"]
-#             b_db = ["Mysterious", "Enhanced","Weakened"]
-#             cur_itm = random.choice(list(itms.keys()))
-#             cur_attribute = random.choice(attributes)
-#             cur_b_db = random.choice(b_db) if random.randint(0,10) > 8 else None
-#             if cur_attribute == "Strength":
-#                 self.attack_min += itms[cur_itm]
-#             elif cur_attribute == "Health":
-#                 self.hp += itms[cur_itm]
-#             else:
-#                 self.luck += 0.4
-#             item = f"{cur_b_db} {cur_itm} of {cur_attribute}"
-#             self.list_of_items.append(item)
-#             typeText(f"\nYou've aquired {item}\n",1)
-#         else:
-#             typeText("\nThis thing had nothing on it\n",1)
+class Item:
+    
+    items = {"attack_items":["Sword", "Gauntlet", "Dagger"],
+             "defense_items":["Shield", "Helmet", "Chestplate"],
+             "health_items":["Herbs", "Bandages", "Health Potion"],
+             "general_items":["Necklace", "Stone", "Fruit"]}
+    
+    item_attributes = {"Strength":Player.min_attack, 
+                       "Power":Player.max_attack, 
+                       "Health":Player.health, 
+                       "Luck":Player.luck, 
+                       "Defence":Player.defense}
+    
+    item_qualities = {"Weakened":1, "":5, "Enhanced":10, "Mysterious":100}
+    
+    @classmethod
+    def getRandomItem(cls):
+        item = random.choice(cls.items[random.choice(cls.items.keys())])
+        item_attribute = random.choice(cls.item_attributes)
+        item_quality = random.choice(cls.item_qualities)
+        #Finish please
 
 class Room():
-    element = []
+    chest_luck = ["filled", "empty"]
+    element = ""
     
     @staticmethod
     def enterStartRoom():  
@@ -113,6 +120,9 @@ class Room():
                 
         Player.step()
 
+    def enterChestRoom():
+        pass
+    
 
 # class Enemy:
 #     def __init__(self, element, enemy_type, hp, min_attack, max_attack):
